4acc961f49c405ab6a8714fb698334295611de77
Blake McAvoy <120762820+bamcavoy@users.noreply.github.com>
2024-02-16 16:08:39 -0700

    connect to sqlite

---
+++ Mission06mvc/Models/MovieListContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission06mvc.Models
+{
+    public class MovieListContext : DbContext
+    {
+        public MovieListContext(DbContextOptions<MovieListContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
--- Mission06mvc/Program.cs
+++ Mission06mvc/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Mission06mvc.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MovieListContext>(options =>
+{
+    options.UseSqlite(builder.Configuration["ConnectionStrings: SQLiteConnection"]);
+});
 
 var app = builder.Build();
 
